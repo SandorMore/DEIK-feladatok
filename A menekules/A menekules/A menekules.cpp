256b1a061d15a83c3e99a4256193bbeabcec8d19
SandorMore <msaso0725@gmail.com>
2025-11-27 18:47:43 +0100

    masodik ciklus

---
+++ A menekules/A menekules/A menekules.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <fstream>
+#include <math.h>
+#include <cstdlib>
+#include <vector>
+
+int main()
+{
+    std::fstream fs("Text.txt");
+    std::ofstream os("Output.txt");
+    int lab = 1;
+    int tud = 1;
+    std::vector<char> p;
+    for (char c; fs >> c;) {
+        p.emplace_back(c);
+    }
+
+    char last = p[0];
+    int c{1};
+    for (int i = 1; i < p.size(); ++i) {
+        if (p[i] == 'P') {
+            os << "1";
+            return 0;
+        }
+        if (p[i] == 'l') {
+            ++lab;
+            goto forEnd;
+        }
+        if (p[i] == 'c') {
+            ++tud;
+			goto forEnd;
+        }
+        if (p[i] == '_') {
+            c = 0;
+			goto forEnd;
+        }
+        if (p[i] == last) {
+            c++;
+            continue;
+        }
+
+        switch (p[i])
+        {
+            case '~':
+                if (tud < c)
+                    goto end;
+                break;
+            case '|':
+                if (lab < c)
+                    goto end;
+                break;
+            default:
+                break;
+        }
+
+        forEnd:
+        last = p[i];
+        c = 1;
+    }
+    end:
+    os << "0";
+    fs.close();
+
+    return 0;
+
+}
